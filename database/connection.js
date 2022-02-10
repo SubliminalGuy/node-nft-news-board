06ab6e330389e61c010f2ff08bc9a8d8364f124a
Subliminal Guy <subliminal_kid@posteo.de>
2022-02-10 14:55:13 +0100

    new removed

--- database/connection.js
+++ database/connection.js
@@ -6,7 +6,7 @@ var dbUser = process.env.DB_USER;
 var dbPassword = process.env.DB_PASSWORD;
 var dbHost = process.env.DB_HOST;
 // Setup default mongoose connection
-//var mongoDB = "mongodb://SubGuy:nAmEtHYstAtW@odinproj1-shard-00-00.32enl.mongodb.net:27017,odinproj1-shard-00-01.32enl.mongodb.net:27017,odinproj1-shard-00-02.32enl.mongodb.net:27017/NewsBoard?ssl=true&replicaSet=atlas-5gf361-shard-0&authSource=admin&retryWrites=true&w=majority"
+
 var mongoDB = `mongodb://${dbUser}:${dbPassword}@odinproj1-shard-00-00.32enl.mongodb.net:27017,odinproj1-shard-00-01.32enl.mongodb.net:27017,odinproj1-shard-00-02.32enl.mongodb.net:27017/NewsBoard?ssl=true&replicaSet=atlas-5gf361-shard-0&authSource=admin&retryWrites=true&w=majority`
 mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true})
 
